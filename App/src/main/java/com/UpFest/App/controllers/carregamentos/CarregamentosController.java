b383119ebc3d87140b13019c632652b137cd86fa
António <oantoniomauricio@gmail.com>
2023-10-09 19:08:31 +0100

    add endpoint /cashless/:id_evento/saldo?participante=joao.sousa@gmail.com

--- App/src/main/java/com/UpFest/App/controllers/carregamentos/CarregamentosController.java
+++ App/src/main/java/com/UpFest/App/controllers/carregamentos/CarregamentosController.java
@@ -1,5 +1,9 @@
 package com.UpFest.App.controllers.carregamentos;
 
+import com.UpFest.App.entities.ContaCashless;
+import com.UpFest.App.services.cashless.carregamentos.CarregamentosServiceImp;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.*;
@@ -9,9 +13,18 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/cashless")
 public class CarregamentosController {
 
+    @Autowired
+    CarregamentosServiceImp carregamentosServiceImp;
+
     @GetMapping("/{id_evento}/saldo")
     public ResponseEntity<?> getSaldoFromParticipante(@PathVariable Long id_evento, @RequestParam String participante) {
 
-        return null;
+        try {
+            ContaCashless contaCashless = carregamentosServiceImp.getSaldoFromParticipante(id_evento, participante);
+            return ResponseEntity.status(HttpStatus.OK).body(contaCashless.getValor_atual() + " euros.");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
+
     }
 }
--- App/src/main/java/com/UpFest/App/services/cashless/carregamentos/CarregamentosServiceImp.java
+++ App/src/main/java/com/UpFest/App/services/cashless/carregamentos/CarregamentosServiceImp.java
@@ -3,6 +3,7 @@ package com.UpFest.App.services.cashless.carregamentos;
 import com.UpFest.App.entities.ContaCashless;
 import com.UpFest.App.entities.Evento;
 import com.UpFest.App.entities.Participante;
+import com.UpFest.App.repositories.cashless.ContaCashlessRepository;
 import com.UpFest.App.repositories.evento.EventoRepository;
 import com.UpFest.App.repositories.venda.ParticipanteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +19,9 @@ public class CarregamentosServiceImp implements CarregamentosService {
 
     @Autowired
     ParticipanteRepository participanteRepository;
+
+    @Autowired
+    ContaCashlessRepository contaCashlessRepository;
 
     @Override
     public ContaCashless getSaldoFromParticipante(Long id_evento, String emailParticipante) throws Exception {
@@ -38,9 +42,17 @@ public class CarregamentosServiceImp implements CarregamentosService {
             throw new Exception("O participante com o email " + emailParticipante + " não existe.");
         }
 
+        Long id_participante = participanteFromReq.get().getId();
 
+        //
+        //
+        Optional<ContaCashless> contaCashlessFromUser = contaCashlessRepository.findById(id_participante);
 
-        return null;
+        if (!eventFromReq.isPresent()) {
+            throw new Exception("A conta cashless com o id de participante " + id_participante + " não existe.");
+        }
+
+        return contaCashlessFromUser.get();
 
     }
 }
